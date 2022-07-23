ebc4bce39c90cc60491ba9e8cb20d996e6752a0d
ryanshappa <89917797+ryanshappa@users.noreply.github.com>
2022-07-23 13:56:15 -0500

    Add files via upload

---
+++ road.js
@@ -0,0 +1,31 @@
+class Road{
+    constructor(x,width,laneCount=3){
+        this.x=x;
+        this.width=width;
+        this.laneCount=laneCount;
+        
+        this.left=x-width/2;
+        this.righ=x+width/2;
+
+        const infinity=1000000;
+        this.top=-infinity;
+        this.bottom=infinity;
+    }
+
+    draw(ctx){
+        ctx,lineWidth=5;
+        ctx.strokeStyle="white";
+
+        ctx.beginPath();
+        ctx.moveTo(this.left,this.top);
+        ctx.lineTo(this.left,this.bottom);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.righyt,this.top);
+        ctx.lineTo(this.right,this.bottom);
+        ctx.stroke();
+
+    }
+
+}
